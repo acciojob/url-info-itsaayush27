d6523588f41b9c81ec81f7328fa2922c4aca36d5
Aayush Gour <138443486+itsaayush27@users.noreply.github.com>
2025-08-31 16:09:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const currentUrl = window.location.href;
+
+// Get its length
+const urlLength = currentUrl.length;
+
+// Show result
+alert("The length of the URL is: " + urlLength);
